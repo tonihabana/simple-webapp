65120efef7476df3cd2143a96beb4ea30dc85679
toni <amdcsp@gmail.com>
2012-10-07 11:21:55 +0200

    Initial commit.

---
+++ src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
@@ -0,0 +1,16 @@
+package org.sonatype.mavenbook.web;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SimpleServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws ServletException, IOException {
+	PrintWriter out = response.getWriter();
+	out.println( "SimpleServlet is now being executed, take a look!" );
+        out.flush();
+        out.close();
+    }
+}
