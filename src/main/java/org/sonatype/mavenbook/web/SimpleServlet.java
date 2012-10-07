f181380bfb7f2242ade7e26472c202b69049cb01
toni <amdcsp@gmail.com>
2012-10-07 11:38:21 +0200

    Breaking compilation on purpose.

--- src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
+++ src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class SimpleServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request,
+    public static void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
 	PrintWriter out = response.getWriter();
